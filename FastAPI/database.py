aad9e5558fa1612134be0f10ba5cbf4627bd1e56
Yenuli Chamodya <yenulichamodya@gmail.com>
2024-03-05 16:30:31 +0530

    Update database.py

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -8,3 +8,5 @@ URL_DATABASE= 'sqlite:///./finance.db'
 engine =  create_engine(URL_DATABASE, connect_args={"check_same_thread":False}) 
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
