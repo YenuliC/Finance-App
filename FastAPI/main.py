d5b5df886178c00866717fa973266965409b7337
Yenuli Chamodya <yenulichamodya@gmail.com>
2024-03-09 19:22:46 +0530

    Create the backend

---
+++ FastAPI/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI,  Depends, HTTPException
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import engine, SessionLocal
+import models 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = {
+    'http://localhost:3000'
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
+class TransactionBase(BaseModel):
+    amount:float
+    category:str
+    description:str
+    is_income:bool
+    date:str
+    
+class TransactionModel(TransactionBase):
+    id:int
+    
+    class Config:
+        from_attributes = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]   
+
+models.Base.metadata.create_all(bind=engine)
+
+@app.post("/transaction", response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db:db_dependency):
+    db_transaction = models.Transaction(**transaction.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+@app.get("/transactions", response_model=List[TransactionModel])
+async def read_transactions(db:db_dependency, skip:int=0, limit:int=100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions
+    
+    
