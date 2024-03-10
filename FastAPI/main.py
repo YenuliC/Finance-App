66af48d80005ffb62b4c6f92fad5aba360011ee9
Yenuli Chamodya <yenulichamodya@gmail.com>
2024-03-11 01:47:31 +0530

    Create the frontend part.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -9,12 +9,15 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = {
-    'http://localhost:3000'
+    'http://localhost:3000',
 }
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
 class TransactionBase(BaseModel):
@@ -41,7 +44,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.post("/transaction", response_model=TransactionModel)
+@app.post("/transactions/", response_model=TransactionModel)
 async def create_transaction(transaction: TransactionBase, db:db_dependency):
     db_transaction = models.Transaction(**transaction.dict())
     db.add(db_transaction)
@@ -49,7 +52,7 @@ async def create_transaction(transaction: TransactionBase, db:db_dependency):
     db.refresh(db_transaction)
     return db_transaction
 
-@app.get("/transactions", response_model=List[TransactionModel])
+@app.get("/transactions/", response_model=List[TransactionModel])
 async def read_transactions(db:db_dependency, skip:int=0, limit:int=100):
     transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
     return transactions
